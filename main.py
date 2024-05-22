9441b92efd54d834b6c18bb46dfad2399b693d09
Wiktor Musiałowski <w.musialowski01@gmail.com>
2024-05-22 22:39:56 +0200

    porządkowanie danych do dalszej analizy

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 import pandas as pd
 
 # Wczytanie danych
-url = "https://gist.githubusercontent.com/Wiktor0000/73a321d9dacfa3f67e05cef585001532/raw/5213dbcca77a21e8d00a8e0201ca488eea9b4f23/gistfile1.txt"
+url = "https://gist.githubusercontent.com/Wiktor0000/6860ca399b57170d2d2c423d442e5037/raw/b7b571f2fc52d3ab6a9e6de2b58d99186a92fbad/data_project.csv"
 data_frame = pd.read_csv(url, sep=';')
+
+data_frame.columns = data_frame.columns.str.replace(' _', '_')
+data_frame["zanieczyszczenie_powietrza"] = data_frame["zanieczyszczenie_powietrza"].str.replace(',', '.').astype(float)
+data_frame["liczba_osob_z_wyksztalceniem_wyzszym_(%)"] = data_frame["liczba_osob_z_wyksztalceniem_wyzszym_(%)"].str.replace(',', '.').astype(float)
 
 pd.set_option('display.max_rows', None)
 pd.set_option('display.max_columns', None)
 
 print(data_frame)
+
+average_pollution = data_frame["zanieczyszczenie_powietrza"].mean()
+print(f"Average pollution: {average_pollution:.2f}")
